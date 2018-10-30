87833c6ca98fc2b508ef69c214da0d218ab6d188
Gulzat Karimova and Patricia Filipovic <patricia@>
2018-10-30 15:20:02 -0700

    debugging and updated

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,17 @@ function Ticket(age, movie, time) {
 Ticket.prototype.ticketPrice = function() {
   var price = 10
   if (this.age > "65" || this.age < "12") {
-    price -= 2;
+    price -= 2 ;
   }
   if (this.time <= 1400) {
     price -= 3;
   } else if (this.time <= 1700 && this.time > 1400) {
     price -= 1;
   }
- console.log(price);
+  return price;
 }
-// Ticket.prototype.findMovie = function() {
-//   if (this.inputtedMovies == "0") {
-//     this.movie = "A Star Is Born";
-//   }
-//   else if (this.inputtedMovies == "1") {
-//       this.movie = "Halloween";
-//   }
-//    else if (this.inputtedMovies == "2") {
-//      this.movie = "Venom";
-//   }
-//   else if (this.inputtedMovies == "3") {
-//     this.movie = "Smallfoot";
-//   }
-//    return this.movie;
-// }
+
 // User Interface Logic ---------
-
-
 $(document).ready(function() {
   $("form#tickets").submit(function(event) {
     event.preventDefault();
@@ -42,13 +26,17 @@ $(document).ready(function() {
     var inputtedTime = $("input#time").val();
     var inputtedMovies = $("#movie").attr('selected',true).val();
 
+    $("input#age").val("");
+    $("input#time").val("");
+    $("#movie").val("");
+
     var newTicket = new Ticket(inputtedAge, inputtedMovies, inputtedTime);
     $(".show-price").show();
     $(".movie").html(inputtedMovies);
     $(".time").html(inputtedTime);
+    
+    var totalPrice = newTicket.ticketPrice()
+    $(".output").html("Your total cost is: $" + totalPrice);
 
-    var totalPrice = newTicket.ticketPrice(inputtedAge, inputtedTime)
-    $(".output").html(totalPrice);
-    console.log(inputtedAge, inputtedTime);
   });
 });
